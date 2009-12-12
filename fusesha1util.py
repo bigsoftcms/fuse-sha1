a83de846f67293f113a4517e38b1e173fd97951b
Chris Bouzek <coldfusion78@gmail.com>
2009-12-12 13:17:21 -0800

    Added a moveFile method.  This is used for moving duplicates.

--- fusesha1util.py
+++ fusesha1util.py
@@ -1,6 +1,6 @@
 import hashlib
 # Calculate an SHA1 hex digest for a file
-# Copyright (C) 2009 Chris Bouzek  <coldfusion78@gmail.com>
+# Copyright (C) 2009 Chris Bouzek  <bouzekc@gmail.com>
 #
 #
 #    This program can be distributed under the terms of the GNU LGPL.
@@ -8,6 +8,7 @@ import hashlib
 #
 
 import logging
+import os
 
 LOG_FILENAME = "LOG"
 logging.basicConfig(filename=LOG_FILENAME,level=logging.INFO,)
@@ -24,6 +25,23 @@ def sha1sum(fobj):
 			break
 		m.update(d)
 	return m.hexdigest()
+	
+def moveFile(path, dstdir):
+	"""Moves the file with the given path to the dstdir, removing any common prefixes between path 
+	and dstdir.  After moving, this will remove the parent directory for path if it is empty."""
+	newpath = os.path.abspath(path)
+	dstdir = os.path.abspath(dstdir)
+	# remove any common prefixes so that we can create a directory structure
+	prefix = os.path.commonprefix([dstdir, newpath])
+	if len(prefix) > 1: newpath = newpath.replace(prefix, '', 1)
+	newpath = os.path.join(dstdir, newpath)
+	newparent = os.path.dirname(newpath)
+	if not os.path.exists(newparent): os.makedirs(newparent)
+	if not os.path.exists(newpath): os.rename(path, newpath)
+	
+	oldparent = os.path.dirname(path)
+	if len(os.listdir(oldparent)) <= 0:
+		os.rmdir(oldparent)
 	
 # Wraps a code block so that if an exception occurs, it is logged
 class ewrap:
